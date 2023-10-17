9b74237fb874c258c405c1be22b47a3be23470bc
Vitor Dias <temadark2022@gmail.com>
2023-10-17 03:10:18 -0300

    Corrigindo alguns erros
    Corrigindo erros que ta dando no React router

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from "./paginas/Home";
 import Empresa from "./paginas/Empresa";
 import Contato from "./paginas/Contato";
@@ -7,22 +7,20 @@ import NavBar from "./paginas/NavBar";
 function App() {
 
   return (
-    <div>
-      <Router>
-        <NavBar/>
-        <Switch>
-        <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route path="/empresa">
-            <Empresa/>
-          </Route>
-          <Route path="/contato">
-            <Contato/>
-          </Route>
-        </Switch>
-      </Router>
-      </div>
+    <Router>
+      <NavBar/>
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route path='/empresa'>
+          <Empresa/>
+        </Route>
+        <Route path='/contato'>
+          <Contato/>
+        </Route>
+      </Switch>
+    </Router>
           )
 }
 
